d9c9e6cb69644e950bac7794a81a9184ba4dfadb
fbarzola <135671763+fbarzola@users.noreply.github.com>
2023-06-05 21:15:09 -0300

    Control de Accesos
    CONTROL DE ACCESOS
    
    Sistema de registro de entrada y salida de
    invitados o residentes del establecimiento, 
    con asignacion de tarjeta(id) para molinetes.
    
    FLUJO:
    lector de codigos de barra
    leer DNI
    se completan los datos automaticamente en el form.
    Base de datos con invitados y residentes habilitados.
    Asignacion de tarjeta RFID, fecha/hora inicio
    Conexion con lector RFID y molinete.

---
+++ script.js
@@ -0,0 +1,34 @@
+document.getElementById('access-form').addEventListener('submit', function(event) {
+    event.preventDefault(); // Evita que el formulario se envíe
+  
+    // Obtener los valores de los campos de entrada
+    var dni = document.getElementById('dni').value;
+    var nombre = document.getElementById('nombre').value;
+    var apellido = document.getElementById('apellido').value;
+    var fechaNacimiento = document.getElementById('fecha-nacimiento').value;
+    var nacionalidad = document.getElementById('nacionalidad').value;
+    var sexo = document.getElementById('sexo').value;
+  
+    // Validación de campos
+    if (dni === '' || nombre === '' || apellido === '' || fechaNacimiento === '' || nacionalidad === '' || sexo === '') {
+      alert('Por favor, completa todos los campos.');
+      return;
+    }
+  
+    // Validación de fecha de nacimiento
+    var regexFecha = /^\d{2}\/\d{2}\/\d{4}$/;
+    if (!regexFecha.test(fechaNacimiento)) {
+      alert('La fecha de nacimiento debe tener el formato DD/MM/AAAA.');
+      return;
+    }
+  
+    // Validación de DNI (solo números)
+    var regexDNI = /^\d+$/;
+    if (!regexDNI.test(dni)) {
+      alert('El DNI debe contener solo números.');
+      return;
+    }
+  
+    // Aquí puedes realizar más validaciones o enviar los datos al servidor
+  });
+ 
